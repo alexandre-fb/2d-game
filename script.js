819c04b0da206db723fd2f7c5bb4595279bdc476
Alexandre Bisognin <alexandrefb@planmkt.com.br>
2024-09-15 16:28:51 -0300

    fix: button and style

--- script.js
+++ script.js
@@ -845,17 +845,26 @@ window.addEventListener("load", function () {
   }
 
   const game = new Game(canvas.width, canvas.height);
+  let gameIsStarted = false;
+  const buttonStartGame = document.getElementById("button-start-game");
+  buttonStartGame.addEventListener("click", function () {
+    gameIsStarted = true;
+    this.style.display = 'none'
+    startGame();
+  });
 
-  let lastTime = 0;
-  // animation loop
-  function animate(timeStamp) {
-    const deltaTime = timeStamp - lastTime;
-    lastTime = timeStamp;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    game.draw(ctx);
-    game.update(deltaTime);
-    requestAnimationFrame(animate);
-  }
+  const startGame = () => {
+    let lastTime = 0;
+    // animation loop
+    function animate(timeStamp) {
+      const deltaTime = timeStamp - lastTime;
+      lastTime = timeStamp;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      game.draw(ctx);
+      game.update(deltaTime);
+      requestAnimationFrame(animate);
+    }
 
-  animate(0);
+    animate(0);
+  };
 });
